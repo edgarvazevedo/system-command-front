283d84f45bf6730f16c6f42308f1d186c0fb8ffa
Elizeu Santos <elyzeu.tec@gmail.com>
2021-12-13 16:47:43 -0300

    Product Details Actualizad

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Home from "../components/Home";
 import Signup from "../components/Signup";
 import Login from "../components/Login";
 import Navbar from "./Navbar";
+import ProductDetail from "../product/ProductDetail";
 
 import ProtectedRoute from "../components/ProtectedRoute";
 import { AuthContextComponent } from "../contexts/authContext";
@@ -18,6 +19,10 @@ function App() {
         <Route path="/" element={<ProtectedRoute component={Home} />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route
+              path="/product/:id"
+              element={<ProtectedRoute component={ProductDetail} />}
+            />
       </Routes>
     </AuthContextComponent>
   );
